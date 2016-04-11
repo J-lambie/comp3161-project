a98e7ebf4916d2833178ec8b4dbb89ff8cf62be9
Travis Smith <travissmith94@hotmail.com>
2016-04-11 00:03:05 -0500

    Can insert a user

--- populate_database.py
+++ populate_database.py
@@ -1,3 +1,23 @@
 import MySQLdb
+
+def execute(db , command):
+    cur = db.cursor()
+    try:
+        cur.execute(command)
+        db.commit()
+    except:
+        db.rollback()
+    finally:
+        db.close()
+
 if __name__ == '__main__':
-    print 'Hello'
+    HOST = 'localhost'
+    USER = 'root'
+    PASSWORD = ''
+    DATABASE = 'meal_plan'
+
+    db = MySQLdb.connect(HOST, USER, PASSWORD ,DATABASE)
+
+    print execute(db, "insert into users values ('travissmith94@hotmail.com', 'babbi3', 'Travis', 'Smith', 1994)")
+    
+
