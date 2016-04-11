da85a20e845748ba7f9f4ea4d57a2e25515ca41b
Travis Smith <travissmith94@hotmail.com>
2016-04-11 14:54:05 -0500

    Modified table creation

--- database.sql
+++
@@ -1,8 +0,0 @@
-create table users(
-  email varchar(320) not null,
-  password varchar(30) not null,
-  firstname varchar(30) not null,
-  lastname varchar(30) not null,
-  year_of_birth year not null,
-  primary key(email)
-);
