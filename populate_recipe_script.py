081812131f66fb320c62bf0ccd7e40d99b66af60
j_lambie <jillianjohnson9957@gmail.com>
2016-04-13 17:49:55 +0000

    creation of recipes scripts works

--- populate_recipe_script.py
+++ populate_recipe_script.py
@@ -3,6 +3,8 @@ from sys import argv, exit
 from faker import Factory
 import random, string
 
+verbs = ['baked', 'boiled', 'chopped', 'sautee']
+
 f = open('foods.txt', 'r')
 g = open('drinks.txt', 'r')
 foods = []
@@ -22,7 +24,7 @@ g.close()
 
 def insert_recipe(db, recipe):
 	fil = open('recipes.sql', 'a')
-	command = "insert into recipes (NULL, '%s', '%s', '%d', '%s')" % (recipe['email'], recipe['recipe_name'], recipe['calories'], recipe['image_url'])
+	command = "insert into recipes VALUES (NULL, '%s', '%s', '%d', '%s')" % (recipe['email'], recipe['recipe_name'], recipe['calories'], recipe['image_url'])
 	#print command
 	fil.write(command+";\n")
 	fil.close()
@@ -33,7 +35,7 @@ if __name__ == '__main__':
 	FAKE_EMAIL = '@fake.com'
 
 	if len(argv) < 3:
-		print "Usage python populate_recipe_script.py <table_name> <count>"
+		print "Usage python populate_recipe_script.py <table_name> <count> - To obtain 1,000,000: count = 1000"
 		exit(0)
 	else:
 		table = argv[1]
@@ -45,21 +47,19 @@ if __name__ == '__main__':
 	def create_fake_recipe(ind, ind1):
 		fake = Factory.create()
 		email = "%s%s" % (randomword(10), FAKE_EMAIL)
-		recipe_name = foods[ind] + " and " + drinks[ind1]
+		recipe_name = verbs[random.randint(0,3)] + " " + foods[ind] + " and " + drinks[ind1]
 		calories = random.randint(0,1000);
 		image_url = fake.word() + ".com/" + fake.word() + ".jpg" 
 		recipe = {'email':email, 'recipe_name':recipe_name, 'calories':calories, 'image_url':image_url}
-		return insert_recipe(db, recipe)
+		return insert_recipe('meal_plan', recipe)
 
 	def populate_recipes_table(count):
 		created = 0
-		j = 0
 		while created < count:
+			j = 0
 			while j < count:
-				if not create_fake_recipe(created, j):
-					break
-				else:
-					j += 1
+				create_fake_recipe(created, j)
+				j += 1
 			created += 1
 
 	ACTIONS = {'recipes': populate_recipes_table}
